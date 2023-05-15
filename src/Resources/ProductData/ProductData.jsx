ba2a4b6178ea73e11b2d47b1aad935f9dbc0c44d
Sam15-code <ibtissam.dxb@gmail.com>
2023-05-15 11:49:32 -0700

    redux module 22

---
+++ src/Resources/ProductData/ProductData.jsx
@@ -0,0 +1,48 @@
+export const ProductData = [
+  {
+    id: "1",
+    image:
+      "https://savemartonline.com/wp-content/uploads/2021/09/Sac-A-Main-Femme-3-Pieces-PU-Leather-Tote-Bag-For-Women-Luxury-Tassel-Hand-Bag.jpg",
+    name: "Ladies Bags",
+    price: 860.0,
+    discountedPrice: 850.0,
+    category: ["Living Room", "Home Office"],
+    freeShipping: true,
+    onSale: true,
+    features: {
+     
+    },
+    
+    reviews: [],
+    description:
+      "Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit sed",
+  },
+  {
+    id: "2",
+    image:
+      "https://media.istockphoto.com/id/504742864/photo/stylish-business-clothing-for-businessman.jpg?s=612x612&w=0&k=20&c=AsGrhEMNkmpwqaJPBSACPthMuBsmsDIecRkdFXKSnl0=",
+    name: "Mens Wears",
+    price: 1200.0,
+    category: ["Living Room", "Home Office"],
+    discountedPrice: 1080.0,
+    freeShipping: false,
+    onSale: false,
+    reviews: [],
+    description:
+      "Nemo malesuada animi consectetur, cras consectetuer laborum tenetur, cum, lacus nemo imperdiet facilisis! Aute metus, lorem primis",
+  },
+  {
+    id: "3",
+    image:
+      "https://post.healthline.com/wp-content/uploads/2021/05/apples-1200x628-facebook.jpg",
+    name: "Apples",
+    price: 1200.0,
+    category: ["Living Room", "Home Office"],
+    discountedPrice: 1080.0,
+    freeShipping: false,
+    onSale: false,
+    reviews: [],
+    description:
+      "Nemo malesuada animi consectetur, cras consectetuer laborum tenetur, cum, lacus nemo imperdiet facilisis! Aute metus, lorem primis",
+  },
+];
